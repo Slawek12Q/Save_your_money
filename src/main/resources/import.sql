36eb6d26fb63ff4dda7c1d87fafc811b11588e7b
Slawek12Q <slawek.guzik@interia.pl>
2019-04-05 17:59:35 +0200

    add spring security

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,3 @@
-INSERT INTO `test`.`book` (`title`, `author`, `isbn`) VALUES ('Krzyżacy', 'Henryk Sienkiewicz', '123456789');
-INSERT INTO `test`.`book` (`title`, `author`, `isbn`) VALUES ('Dziady', 'Adam Mickiewicz', '123434567');
-INSERT INTO `test`.`book` (`title`, `author`, `isbn`) VALUES ('Dym', 'Maria Konopnicka', '987654321');
+-- INSERT INTO `test`.`book` (`title`, `author`, `isbn`) VALUES ('Krzyżacy', 'Henryk Sienkiewicz', '123456789');
+-- INSERT INTO `test`.`book` (`title`, `author`, `isbn`) VALUES ('Dziady', 'Adam Mickiewicz', '123434567');
+-- INSERT INTO `test`.`book` (`title`, `author`, `isbn`) VALUES ('Dym', 'Maria Konopnicka', '987654321');
